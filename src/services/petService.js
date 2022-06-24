e8eae57f130e960e3c97224172196037a541efcf
Marc-cherier <cherier.marc@gmail.com>
2022-06-24 16:47:13 +0200

    single card done and buttons done

--- src/services/petService.js
+++ src/services/petService.js
@@ -12,7 +12,7 @@ const petsPerPage = '5';
 export default {
 
     // on récupère une liste de recette
-    getPet(page, selectedSpeciesList) {
+    getPet(page, selectedRecipeTypeList, selectedIngredientList) {
         // toutes les requêtes vont tenir compte du nombre de résultats par page
         let queryString = '?per_page=' + petsPerPage + '&page=' + page;
         // gestion du filtre recipeType :
@@ -20,16 +20,16 @@ export default {
         // nomdelataxo[operator]=RELATION (OR ou AND, exlusif ou non)
         // on veut ajouter des paramètres dans l'URL : ?recipe_type[terms]=18,17&recipe_type[operator]=AND
         // on ne veut appliquer ces filtres que dans le cas où on a sélectionné des types de recettes dans le select multiple.
-        // if (selectedRecipeTypeList.length > 0) {
-        //     // join() permet de rassembler les éléments d'un array dans une seule chaîne de caractère avec le séparateur fourni en argument
-        //     const recipeTypeTerms = selectedRecipeTypeList.join(',');
-        //     queryString += '&recipe_type[terms]=' + recipeTypeTerms + '&recipe_type[operator]=AND'
-        // }
+        if (selectedRecipeTypeList.length > 0) {
+            // join() permet de rassembler les éléments d'un array dans une seule chaîne de caractère avec le séparateur fourni en argument
+            const recipeTypeTerms = selectedRecipeTypeList.join(',');
+            queryString += '&pet_type[terms]=' + recipeTypeTerms + '&pet_type[operator]=AND'
+        }
 
         // idem avec le filtre ingredient
-        if (selectedSpeciesList > 0) {
-            const speciesTerms = selectedSpeciesList.join(',');
-            queryString += '&species[terms]=' + speciesTerms + '&species[operator]=AND'
+        if (selectedIngredientList.length > 0) {
+            const ingredientTerms = selectedIngredientList.join(',');
+            queryString += '&ingredient[terms]=' + ingredientTerms + '&ingredient[operator]=AND'
         }
 
         // axios.get() retourne une Promise => on attend que cette méthode réponde, puis on déclenche then() en cas de succès, et catch() en cas d'erreur.
@@ -38,16 +38,16 @@ export default {
     },
 
     // récupérer les données d'une recette par son id
-    getSinglePet(petId) {
-        return axios.get(baseUrl + endpoint + '/' + petId + '?_embed');
+    getSinglePet(alertId) {
+        return axios.get(baseUrl + endpoint + '/' + alertId + '?_embed');
     },
 
 
-    createPet(petData) {
+    createPet(recipeData) {
 
         return axios.post(
             baseUrl + endpoint,
-            petData, // on passe directement l'objet recu en paramètre et qui contient les valeurs saisies dans le formulaire
+            animalData, // on passe directement l'objet recu en paramètre et qui contient les valeurs saisies dans le formulaire
             {
                 headers: {
                     "Authorization": "Bearer " + localStorage.getItem('token')
