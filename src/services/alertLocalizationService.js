f2f5e25cef9595891557b7ad080ea1d54f4cd97d
pnobecourt <paul.nobecourt@orange.fr>
2022-06-30 12:15:45 +0200

    localizations select options on alerts page from taxonomy ok

---
+++ src/services/alertLocalizationService.js
@@ -0,0 +1,19 @@
+// on importe les dépendances nécessaires
+import axios from "axios";
+
+// on récupère la constant baseUrl depuis apiClientService
+import { baseUrl } from "./apiClientService";
+
+// on utilise endpoint dédié /wp/v2/ingredient
+const endpoint = "/wp/v2/alert_localization";
+
+// on fait en sorte  de pouvoir "import" cet objet depuis un autre fichier JS
+export default {
+  // on récupère tous les types de recettes
+  getAllAlertLocalizations() {
+    const responsePromise = axios.get(baseUrl + endpoint);
+
+    // on renvoie le résultat de l'appel axios, comme ça on gère la Promise retournée par axios directement à l'endroit où on a fait appel à getAllIngredients()
+    return responsePromise;
+  },
+};
